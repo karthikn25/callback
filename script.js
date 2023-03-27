1c1bf6f7c3cb59626552b2f779845a56150a4ae0
karthikn25 <91930828+karthikn25@users.noreply.github.com>
2023-03-27 10:34:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,33 @@
-var div=document.createElement("div");
-div.setAttribute("class","container");
-
-var h1=document.createElement("h1");
-h1.setAttribute("id","main");
-h1.innerHTML="10";
-
-
-div.append(h1);
-document.body.append(div);
-
-
-
-
-
-
-setTimeout(function numbers(){
-    h1.innerHTML="09"
-   
-},1000)
-setTimeout(function numbers(){
-    h1.innerHTML="08"
-   
-},2000)
-setTimeout(function numbers(){
-    h1.innerHTML="07"
-   
-},3000)
-setTimeout(function numbers(){
-    h1.innerHTML="06"
-   
-},4000)
-setTimeout(function numbers(){
-    h1.innerHTML="05"
-   
-},5000)
-setTimeout(function numbers(){
-    h1.innerHTML="04"
-   
-},6000)
-setTimeout(function numbers(){
-    h1.innerHTML="03"
-   
-},7000)
-setTimeout(function numbers(){
-    h1.innerHTML="02"
-   
-},8000)
-setTimeout(function numbers(){
-    h1.innerHTML="01"
-   
-},9000)
-
-
-setTimeout(function time(numbers){
-    h1.innerHTML="HAPPY INDEPENDENTS DAY"
-},10000)
-
+setTimeout(() => {
+    document.getElementById("main").innerHTML=10;
+    setTimeout(() => {
+        document.getElementById("main").innerHTML=09;
+        setTimeout(() => {
+            document.getElementById("main").innerHTML=08;
+            setTimeout(() => {
+                document.getElementById("main").innerHTML=07;
+                setTimeout(() => {
+                    document.getElementById("main").innerHTML=06;
+                    setTimeout(() => {
+                        document.getElementById("main").innerHTML=05;
+                        setTimeout(() => {
+                            document.getElementById("main").innerHTML=04;
+                            setTimeout(() => {
+                                document.getElementById("main").innerHTML=03;
+                                setTimeout(() => {
+                                    document.getElementById("main").innerHTML=02;
+                                    setTimeout(() => {
+                                        document.getElementById("main").innerHTML=01;
+                                        setTimeout(() => {
+                                            document.getElementById("main").innerHTML="HAPPY INDEPENDENCE DAY";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
